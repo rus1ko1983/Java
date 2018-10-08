a8f37c50602ef973d5cb8f306601bc05b6067132
Ruslan Stepanchenko <ruslan.stepanchenko@astelit.ukr>
2018-10-08 20:33:39 +0300

    was added first class

---
+++ src/my/code/java/Test.java
@@ -0,0 +1,13 @@
+package my.code.java;
+import java.lang.*;
+import java.util.*;
+public class Test {
+
+	public static void main(String[] args) {
+
+   System.out.println("Hello");
+		
+		
+	}
+
+}
